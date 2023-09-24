cfc121ab363bc9798f72b419288d6df4974df2dc
Mario Kovac <mkovac25122512@gmail.com>
2023-09-24 20:06:48 +0200

    Change typescript config

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -2,6 +2,5 @@ import { createContext } from "react";
 
 export const UserContext = createContext({
   username: "",
-  /* tslint:disable:no-unused-variable */
   setUsername: (username: string) => {},
 });
